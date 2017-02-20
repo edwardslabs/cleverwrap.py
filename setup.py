12801076b20a501313ca06cc4c091eb79a8f0122
Andrew Edwards <andrewthomasedwards@gmail.com>
2017-02-20 05:23:51 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
           name = 'cleverwrap',
           packages = ['cleverwrap'],
-          version = '0.2.1',
+          version = '0.2.2',
           description = 'A wrapper for the official cleverbot.com API',
           author = 'Andrew Edwards',
           author_email = 'andrewthomasedwards@gmail.com',
           url = 'https://github.com/edwardslabs/cleverwrap.py',
-          download_url = 'https://github.com/edwardslabs/cleverwrap.py/tarball/0.2',
+          download_url = 'https://github.com/edwardslabs/cleverwrap.py/tarball/0.2.2',
           keywords = ['cleverbot', 'wrapper', 'clever'],
           classifiers = [],
 )
