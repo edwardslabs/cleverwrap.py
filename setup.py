4d4e1731f1a27fa23245a2d7ce1391297a279b2f
Andrew Edwards <andrewthomasedwards@gmail.com>
2017-02-20 12:31:31 -0500

    missing comma

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ setup(
           name = 'cleverwrap',
           packages = ['cleverwrap'],
           license = 'MIT',
-          install_requires = ['requests']
-          version = '0.2.3',
+          install_requires = ['requests'],
+          version = '0.2.3.1',
           description = 'A wrapper for the official cleverbot.com API',
           author = 'Andrew Edwards',
           author_email = 'andrewthomasedwards@gmail.com',
           url = 'https://github.com/edwardslabs/cleverwrap.py',
-          download_url = 'https://github.com/edwardslabs/cleverwrap.py/tarball/0.2.3',
+          download_url = 'https://github.com/edwardslabs/cleverwrap.py/tarball/0.2.3.1',
           keywords = ['cleverbot', 'wrapper', 'clever'],
           classifiers =(
               'Programming Language :: Python :: 3 :: Only',
